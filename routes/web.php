74be57b8225695c181b70086a33c25abb8741ee9
nika39 <nika.shamanauri@gmail.com>
2026-04-19 13:17:13 +0400

    fix: generate Wayfinder routes before npm build in production Dockerfile

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
 Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
+    'canRegister' => false, // Features::enabled(Features::registration())
 ])->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
