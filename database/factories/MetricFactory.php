bd5161f9f7b5e59a5b483935909f8577574a02a1
nika39 <nika.shamanauri@gmail.com>
2026-04-19 20:49:08 +0400

    fix: define default state for Metric model in MetricFactory

--- database/factories/MetricFactory.php
+++ database/factories/MetricFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Metric;
+use App\Models\Site;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +19,13 @@ class MetricFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'site_id' => Site::factory(),
+            'p2p_bytes' => fake()->numberBetween(0, 10_000_000),
+            'http_bytes' => fake()->numberBetween(0, 10_000_000),
+            'recorded_at' => now()->format('Y-m-d_H'),
+            'browser' => fake()->randomElement(['Chrome', 'Firefox', 'Safari']),
+            'os' => fake()->randomElement(['Windows', 'macOS', 'Linux']),
+            'player_version' => fake()->randomElement(['1.0', '1.1', '1.2']),
         ];
     }
 }
